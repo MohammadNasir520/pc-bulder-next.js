77070e658040587c5e0e1d59c154ae9522ea2418
MohammadNasir520 <nasirahsan520@gmail.com>
2023-08-01 05:25:29 +0600

    pc build successfully

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8c47cb2fedbb2a61"; }
+/******/ 		__webpack_require__.h = function() { return "0143f2d4b28bb47f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/pcbuilder.js
+++ src/pages/pcbuilder.js
@@ -1,6 +1,7 @@
 import { Button } from "@material-tailwind/react";
 import Link from "next/link";
 import { useSelector } from "react-redux";
+import toast, { Toaster } from 'react-hot-toast';
 
 
 const PcBuilder = () => {
@@ -43,110 +44,123 @@ const PcBuilder = () => {
 
 
     return (
+        <div>
 
-        <div className="overflow-hidden min-h-screen mx-auto border border-gray-200 shadow-md m-5">
-            <table className="w-4/5 mx-auto border-collapse bg-white text-left text-sm text-gray-500">
-                <thead className="bg-gray-50">
-                    <tr>
-                        <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-                            categories
-                        </th>
-                        <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-                            Added Components
-                        </th>
-
-
-                        <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-                            choose
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-                    {categories?.map((category) => (
-                        <tr key={category?.id} className="hover:bg-gray-50">
-                            <th className="flex gap-3 px-6 py-4 font-normal items-center text-gray-900">
-                                <div className="h-24 w-24">
-                                    <img
-                                        className="h-full w-full  object-cover object-center"
-                                        src={category.image}
-                                        alt=""
-                                    />
-
-                                </div>
-                                <div className="text-sm">
-                                    <div className="font-semibold">{category?.title}</div>
-                                    <div className="font-medium text-gray-700">Required</div>
-                                </div>
+            <div className="overflow-hidden min-h-screen mx-auto border border-gray-200 shadow-md m-5">
+                <table className="w-4/5 mx-auto border-collapse bg-white text-left text-sm text-gray-500">
+                    <thead className="bg-gray-50">
+                        <tr>
+                            <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                                categories
+                            </th>
+                            <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                                Added Components
                             </th>
 
-                            {/* added components */}
-                            {components.find(component => component.category === category.title) ?
-                                components.filter(component => component.category === category.title)?.map(
-                                    component =>
 
-                                        <>
-                                            <td className="px-6 py-4 ">
-                                                <div className="flex items-center gap-1">
-                                                    <div className="h-24 w-24">
-                                                        <img
-                                                            className="h-full w-full  object-cover object-center"
-                                                            src={component.image} alt="" />
-                                                    </div>
-                                                    <div>
-                                                        <div className="font-medium flex items-center gap-x-1">
-                                                            rating:
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                viewBox="0 0 24 24"
-                                                                fill="currentColor"
-                                                                className="-mt-0.5 h-5 w-5 text-yellow-700"
-                                                            >
-                                                                <path
-                                                                    fillRule="evenodd"
-                                                                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                                                                    clipRule="evenodd"
-                                                                />
-                                                            </svg>
-                                                            {component?.rating}
+                            <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                                choose
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+                        {categories?.map((category) => (
+                            <tr key={category?.id} className="hover:bg-gray-50">
+                                <th className="flex gap-3 px-6 py-4 font-normal items-center text-gray-900">
+                                    <div className="h-24 w-24">
+                                        <img
+                                            className="h-full w-full  object-cover object-center"
+                                            src={category.image}
+                                            alt=""
+                                        />
+
+                                    </div>
+                                    <div className="text-sm">
+                                        <div className="font-semibold">{category?.title}</div>
+                                        <div className="font-medium text-gray-700">Required</div>
+                                    </div>
+                                </th>
+
+                                {/* added components */}
+                                {components.find(component => component.category === category.title) ?
+                                    components.filter(component => component.category === category.title)?.map(
+                                        component =>
+
+                                            <>
+                                                <td className="px-6 py-4 ">
+                                                    <div className="flex items-center gap-1">
+                                                        <div className="h-24 w-24">
+                                                            <img
+                                                                className="h-full w-full  object-cover object-center"
+                                                                src={component.image} alt="" />
+                                                        </div>
+                                                        <div>
+                                                            <div className="font-medium flex items-center gap-x-1">
+                                                                rating:
+                                                                <svg
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                    viewBox="0 0 24 24"
+                                                                    fill="currentColor"
+                                                                    className="-mt-0.5 h-5 w-5 text-yellow-700"
+                                                                >
+                                                                    <path
+                                                                        fillRule="evenodd"
+                                                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                                                                        clipRule="evenodd"
+                                                                    />
+                                                                </svg>
+                                                                {component?.rating}
+                                                            </div>
+
+                                                            <p> price:   {component.price}</p>
                                                         </div>
 
-                                                        <p> price:   {component.price}</p>
                                                     </div>
 
-                                                </div>
+                                                    <span className="inline-flex items-center    py-1  font-semibold">
+                                                        {(component?.name)}
+                                                    </span>
 
-                                                <span className="inline-flex items-center    py-1  font-semibold">
-                                                    {(component?.name)}
-                                                </span>
+                                                </td>
 
-                                            </td>
-
-                                        </>
-                                )
-                                :
-                                <>
-                                    <td>
-                                        <h1
-                                            className="text-md text-gray-700 font-semibold"
-                                        > please select {category.title} First
-                                        </h1>
-                                    </td>
-                                </>
-                            }
+                                            </>
+                                    )
+                                    :
+                                    <>
+                                        <td>
+                                            <h1
+                                                className="text-md text-gray-700 font-semibold"
+                                            > please select {category.title} First
+                                            </h1>
+                                        </td>
+                                    </>
+                                }
 
 
-                            <td className="px-6 py-4">
-                                <div className="flex gap-2">
-                                    <Link href={`/productComponents/${category?.title}`}>
-                                        <Button size="sm">Choose</Button>
-                                    </Link>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div >
+                                <td className="px-6 py-4">
+                                    <div className="flex gap-2">
+                                        <Link href={`/productComponents/${category?.title}`}>
+                                            <Button size="sm">Choose</Button>
+                                        </Link>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+            </div >
+            <div className="w-full flex justify-center">
+
+                <Button
+                    onClick={() => toast.success('your PC build successfully')}
+                    disabled={components.length < 6 ? true : false}
+                    className="disabled:bg-green-300 bg-green-700 mx-auto my-3"
+                >
+                    Build you Pc
+                </Button>
+            </div>
+        </div>
     );
 };
 
