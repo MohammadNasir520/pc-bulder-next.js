b6b246d6d1fb99df15185471a3c5cc84c11ba12c
MohammadNasir520 <nasirahsan520@gmail.com>
2023-08-03 14:40:36 +0600

    updated code

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/pcbuilder":["static\u002Fchunks\u002Fpages\u002Fpcbuilder.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fpcbuilder"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- .next/static/jv2hnWT6bfsZam7hKIf2k/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002FproductDetailsPage\u002F[productId]","\u002Fproducts\u002F[productCategory]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
